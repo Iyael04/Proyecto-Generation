3fdf4e5ca27ed0a687a57aaed3c109b5ac23f8d2
Irvin <irvinyael444@gmail.com>
2021-08-11 16:29:05 -0500

    items

---
+++ js/add.js
@@ -0,0 +1,12 @@
+let products = [];
+let total = 0;
+
+function add(product, price) {
+    window.alert('El producto se agrego correctamente');
+    products.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = `Subtotal $${total}`
+}
+function pay() {
+    window.alert(products.join(", \n"))
+}
